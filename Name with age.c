e9b01eb05dce9018540adca6832a93caead1895d
pranjal030404 <pranjalp160@gmail.com>
2024-12-15 00:33:56 +0530

    age

---
+++ Name with age.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+void main()
+{
+    int age;
+    age = 18;
+    printf("My name is Pranjal and i my age is ", age);
+    return 0;
+}
---
+++ age.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+void main()
+{
+    int age;
+    age = 18;
+    printf("My name is Pranjal and i am %d year old ", age);
+}
